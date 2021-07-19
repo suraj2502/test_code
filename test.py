f054a3a66a640958c5dc31825ddbdd69d66597db
surajshukla <surajshukla@penguin>
2021-07-19 17:42:44 +0530

    latest change

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-#get the missing numbers from given range of numbers
+"""#get the missing numbers from given range of numbers
 def get_missing(a,n,l,h):
     s=set(a)
     result=[]
@@ -14,4 +14,37 @@ n=len(a)
 l=0
 h=99
 result= get_missing(a,n,l,h)
-print(result)
+print(result)"""
+
+class Solution(object):
+    def findMissingRanges(self, nums, lower, upper):
+        """
+        :type nums: List[int]
+        :type lower: int
+        :type upper: int
+        :rtype: List[str]
+        """
+        def getRange(lower, upper):
+            if lower == upper:
+                return "{}".format(lower)
+            else:
+                return "{}->{}".format(lower, upper)
+        ranges = []
+        pre = lower - 1
+        
+        for i in range(len(nums) + 1):
+            if i == len(nums):
+                cur = upper + 1
+            else:
+                cur = nums[i]
+            if cur - pre >= 2:
+                ranges.append(getRange(pre + 1, cur - 1))
+                
+            pre = cur
+            
+        return ranges
+
+
+if __name__ == "__main__":
+
+    print (Solution().findMissingRanges([0, 1, 3, 50, 75], 0, 99))
